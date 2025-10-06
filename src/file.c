083f521d96e17555313384165e15967f6ba671d1
Rodrigo Garcia <nibblesmx@gmail.com>
2025-10-05 19:41:40 -0600

    chido1

--- src/file.c
+++ src/file.c
@@ -10,11 +10,19 @@
 
 
 int create_db_file(char *filename) {
-
+    int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd < 0) {
+        return -1; // File open/create error
+    }
+    return fd;
 }
 
 int open_db_file(char *filename) {
-
+    int fd = open(filename, O_RDWR);
+    if (fd < 0) {   
+        return -1; // File open error
+    }
+    return fd; // Success, return file descriptor
 }
 
 
